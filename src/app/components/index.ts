a0f4900b177b1462c9a224673f58e54cfdd1ad4d
lisa13 <lisamanukyan@gmail.com>
2020-02-02 22:41:37 +0400

    changes

--- src/app/components/index.ts
+++
@@ -1,3 +0,0 @@
-export { HeaderComponent } from './header/header.component';
-export { SidePanelComponent } from './side-panel/side-panel.component'
-export { UserCredentialsComponent } from './user-credentials/user-credentials.component';
--- src/app/pages/dashboard/index.ts
+++
@@ -1,2 +0,0 @@
-export { MyDocumentsComponent } from './my-documents/my-documents.component';
-export { TeamDocumentsComponent } from './team-documents/team-documents.component';
